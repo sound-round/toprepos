cafd680c7e3f1d0a5f62aff8e90301dd1c162b10
denis <yudaev1@gmail.com>
2021-12-06 19:19:39 +0300

    Add new tests

--- tests/test_get_top_repos.py
+++ tests/test_get_top_repos.py
@@ -3,18 +3,15 @@ import os
 import pathlib
 import json
 import tempfile
-from toprepos.app import app
+from toprepos.app import app, get_from_cache
 from aioresponses import aioresponses
-
-
-import os
+from toprepos import db
 
 
 USERNAME = 'sound-round'
 FIXTURES_DIR = 'fixtures'
 URL = f'http://test.com/api/top/{USERNAME}?limit=4'
 GITHUB_URL = f'https://api.github.com/users/{USERNAME}/repos?page=1'
-
 GITHUB_API_RESPONSE = 'github_api_response.json'
 EXPECTED = 'expected.json'
 
@@ -40,14 +37,14 @@ def test_client():
     app.config.update(test_config)
 
     with app.test_client() as testing_client:
-        yield testing_client 
+        yield testing_client
 
     os.close(db_fd)
     os.unlink(db_path)
 
 
 def test_get_top_repos(requests_mock, test_client):
-    
+
     requests_mock.get(
         GITHUB_URL,
         content=read(get_fixture_path(GITHUB_API_RESPONSE), 'rb'),
@@ -61,4 +58,30 @@ def test_get_top_repos(requests_mock, test_client):
         )
         response = test_client.get(URL)
         assert response.status_code == 200
-        assert response.get_json() == json.loads(read(get_fixture_path(EXPECTED), 'rb'))
+        assert response.get_json() == json.loads(
+            read(get_fixture_path(EXPECTED), 'rb')
+        )
+
+
+def test_404_not_found(test_client):
+
+    response = test_client.get('')
+    assert response.status_code == 404
+
+
+def test_db():
+    db_fd, db_path = tempfile.mkstemp()
+    test_config = {'TESTING': True, 'DATABASE': db_path}
+    app.config.update(test_config)
+
+    with app.app_context():
+        db.create_tables()
+        db.save_to_cache(USERNAME, json.loads(
+            read(get_fixture_path(EXPECTED), 'rb')
+        ))
+
+        repos = get_from_cache(USERNAME, 123)
+        assert repos == json.loads(read(get_fixture_path(EXPECTED), 'rb'))
+
+    os.close(db_fd)
+    os.unlink(db_path)
