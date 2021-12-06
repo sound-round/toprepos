280d1a6217a47ce0ceb1500423cd57f94762c45a
denis <yudaev1@gmail.com>
2021-12-06 13:22:21 +0300

    Update tests

--- tests/test_get_top_repos.py
+++ tests/test_get_top_repos.py
@@ -1,3 +1,4 @@
+from werkzeug.datastructures import Headers
 import pytest
 import os
 import pathlib
@@ -8,6 +9,8 @@ import tempfile
 from toprepos.app import get_top_repos, app
 from aioresponses import aioresponses
 import nest_asyncio
+import requests_mock
+
 
 
 nest_asyncio.apply()
@@ -16,7 +19,8 @@ nest_asyncio.apply()
 USERNAME = 'sound-round'
 FIXTURES_DIR = 'fixtures'
 URL = f'http://test.com/api/top/{USERNAME}?limit=4'
-GITHUB_URL = f'https://api.github.com/users/{USERNAME}/repos'
+GITHUB_URL = f'https://api.github.com/users/{USERNAME}/repos?page=1'
+
 GITHUB_API_RESPONSE = 'github_api_response.json'
 EXPECTED = 'expected.json'
 
@@ -35,32 +39,25 @@ def get_fixture_path(fixture_name):
     )
 
 
-# @pytest.mark.asyncio
-# async def test_get_top_repos(requests_mock):
-#     requests_mock.get(
-#         GITHUB_URL,
-#         content=read(get_fixture_path(GITHUB_API_RESPONSE), 'rb'),
-#         status_code=200,
-#     )
-
-#     with aioresponses() as mocked:
-#         mocked.get(GITHUB_URL, status=200, body=read(get_fixture_path(GITHUB_API_RESPONSE), 'rb'))
-
-#         with app.test_request_context(f'/api/top/{USERNAME}?limit=4'):
-#             with tempfile.TemporaryDirectory() as tmp_directory_path:
-#                 resp = get_top_repos(USERNAME, tmp_directory_path)
-
-
-#                 assert resp.get_json() == json.loads(read(get_fixture_path(EXPECTED), 'rb'))
-
-
 @pytest.fixture(scope='module')
 def test_client():
     with app.test_client() as testing_client:
         yield testing_client 
 
 
-def test_get_top_repos(test_client):
-    response = test_client.get(URL)
-    assert response.status_code == 200
-    assert response.get_json() == json.loads(read(get_fixture_path(EXPECTED), 'rb'))
+def test_get_top_repos(requests_mock, test_client):
+    requests_mock.get(
+        GITHUB_URL,
+        content=read(get_fixture_path(GITHUB_API_RESPONSE), 'rb'),
+        status_code=200,
+    )
+    with aioresponses() as mocked:
+
+        mocked.get(
+            GITHUB_URL,
+            status=200,
+            body=read(get_fixture_path(GITHUB_API_RESPONSE), 'rb')
+        )
+        response = test_client.get(URL)
+        assert response.status_code == 200
+        assert response.get_json() == json.loads(read(get_fixture_path(EXPECTED), 'rb'))
