635c8eb5f0faf613b8a553bc3b8d21c5e573979f
denis <yudaev1@gmail.com>
2021-12-06 12:56:24 +0300

    Update tests

--- tests/test_get_top_repos.py
+++ tests/test_get_top_repos.py
@@ -4,6 +4,7 @@ import pathlib
 import asyncio
 import aiohttp
 import json
+import tempfile
 from toprepos.app import get_top_repos, app
 from aioresponses import aioresponses
 import nest_asyncio
@@ -34,20 +35,32 @@ def get_fixture_path(fixture_name):
     )
 
 
-async def test_get_top_repos(requests_mock):
-    requests_mock.get(
-        GITHUB_URL,
-        content=read(get_fixture_path(GITHUB_API_RESPONSE), 'rb'),
-        status_code=200,
-    )
+# @pytest.mark.asyncio
+# async def test_get_top_repos(requests_mock):
+#     requests_mock.get(
+#         GITHUB_URL,
+#         content=read(get_fixture_path(GITHUB_API_RESPONSE), 'rb'),
+#         status_code=200,
+#     )
 
-    with aioresponses() as mocked:
-        # mocked.get(GITHUB_URL, status=200, body=read(get_fixture_path(API_TEST), 'rb'))
+#     with aioresponses() as mocked:
+#         mocked.get(GITHUB_URL, status=200, body=read(get_fixture_path(GITHUB_API_RESPONSE), 'rb'))
 
-        with app.test_request_context(f'/api/top/{USERNAME}?limit=4'):
-            resp = get_top_repos(USERNAME)
-
-
-            assert resp.get_json() == json.loads(read(get_fixture_path(EXPECTED), 'rb'))
+#         with app.test_request_context(f'/api/top/{USERNAME}?limit=4'):
+#             with tempfile.TemporaryDirectory() as tmp_directory_path:
+#                 resp = get_top_repos(USERNAME, tmp_directory_path)
 
 
+#                 assert resp.get_json() == json.loads(read(get_fixture_path(EXPECTED), 'rb'))
+
+
+@pytest.fixture(scope='module')
+def test_client():
+    with app.test_client() as testing_client:
+        yield testing_client 
+
+
+def test_get_top_repos(test_client):
+    response = test_client.get(URL)
+    assert response.status_code == 200
+    assert response.get_json() == json.loads(read(get_fixture_path(EXPECTED), 'rb'))
